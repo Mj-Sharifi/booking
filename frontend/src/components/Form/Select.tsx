60b02c043fadab8c5f4722c056407bade4e4f93a
mj-end <mj.sharifimanesh@gmail.com>
2024-09-08 20:23:32 +0330

    Select component move with keyDown

--- frontend/src/components/Form/Select.tsx
+++ frontend/src/components/Form/Select.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { useTranslations } from "next-intl";
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 
 type props = {
   id?: string;
   name?: string;
+  initialValue?:string
   options: {
     value: string | number;
     text: string | number;
@@ -19,6 +20,7 @@ type props = {
 export default function Select({
   id,
   name,
+  initialValue,
   options,
   onChange,
   height,
@@ -41,7 +43,52 @@ export default function Select({
   useEffect(() => {
     options ? setSearchedOptions(options) : setSearchedOptions([]);
   }, [options]);
-  label == "City" && console.log("searchedOptions: ", searchedOptions);
+  // Move with arrows
+  const [currentLi, setCurrentLi] = useState<number>(-1);
+  const ulElement = useRef<HTMLUListElement>(null); // Ref for the scrollable container
+
+  useEffect(() => {
+    
+    const handleKeyDown = (e: KeyboardEvent) => {
+      e.preventDefault();
+      if (options?.length) {
+        const lastIndex = options?.length - 1;
+        switch (e.key) {
+          case "ArrowDown":
+            setCurrentLi((prev) =>
+              prev === -1 || prev === lastIndex ? 0 : prev + 1
+            );
+            break;
+          case "ArrowUp":
+            setCurrentLi((prev) => (prev < 1 ? lastIndex : prev - 1));
+            break;
+          case "Escape":
+            setOpen(false);
+            setCurrentLi(-1);
+            break;
+        }
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown); // Clean up listener
+    };
+  }, [options]);
+  useEffect(() => {
+    if (currentLi !== -1 && ulElement.current) {
+      const currentItem = ulElement.current.children[currentLi] as HTMLElement;
+      if (currentItem) {
+        // Ensure the highlighted item scrolls into view
+        currentItem.scrollIntoView({
+          behavior: "smooth",
+          block: "nearest", // Scrolls to the nearest edge if already partially visible
+        });
+      }
+    }
+  }, [currentLi]);
+
   return (
     <div className="relative">
       <div
@@ -70,7 +117,7 @@ export default function Select({
           onChange={(e) => {
             setText("");
             setValue(e.target.value);
-            console.log(e.target.value);
+
             if (e.target.value.length > 2) {
               let newOptions = options.filter((v) =>
                 String(v.text)
@@ -97,7 +144,8 @@ export default function Select({
       </div>
 
       <ul
-        className={`absolute z-20 top-full duration-300 transition-height overflow-y-auto overflow-x-hidden bg-transparent right-0 left-0 flex flex-col bg-white dark:bg-dark overflow-auto shadow-md rounded-b-lg ${
+        ref={ulElement}
+        className={`text-sm md:text-base absolute z-20 top-full duration-300 transition-height overflow-y-auto overflow-x-hidden bg-transparent right-0 left-0 flex flex-col bg-white dark:bg-dark overflow-auto shadow-md rounded-b-lg ${
           open ? "border-2 border-t-0" : "border-0"
         }  border-dark dark:border-white`}
         style={{ maxHeight: open ? `${height || 160}px` : "0" }}
@@ -113,7 +161,9 @@ export default function Select({
                 setFocus(false);
                 setValue("");
               }}
-              className={`duration-300 py-2 px-3 flex gap-2 cursor-pointer w-full hover:bg-darkblue hover:text-white dark:bg-lightblue dark:hover:text-dark`}
+              className={`duration-300 py-2 px-3 flex gap-2 cursor-pointer w-full hover:bg-darkblue hover:text-white dark:bg-lightblue dark:hover:text-dark ${
+                i == currentLi ? "bg-lightblue dark:bg-darkblue" : ""
+              }`}
             >
               {icon}
               {text}
